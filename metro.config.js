1afc33c76484b6e0d4719612b827a348ad793fbb
farhad0724 <162090888+farhad0724@users.noreply.github.com>
2025-03-20 14:53:51 +0500

    Add sass configuration to metro

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,6 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const defaultConfig = getDefaultConfig(__dirname);
+const { assetExts, sourceExts } = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -6,6 +8,14 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const customConfig = {
+    transformer: {
+      babelTransformerPath: require.resolve('./customTransformer.js'),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'  && ext !== 'scss'),
+          sourceExts: [...sourceExts, 'css', 'svg', 'scss', 'sass', 'cjs', 'json'],
+    },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), customConfig);
